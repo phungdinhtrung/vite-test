d39ece83b2b4a3dd3c4bb40b00646f3881109c47
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2024-05-15 05:31:35 +0700

    Basic test: the first example

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,15 @@
-import { expect, test } from 'vitest'
+import { expect, it } from 'vitest'
 import { sum } from './sum'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+it('adds 1 + 2 to equal 3', () => {
+    // Arrange: khai báo biến a và b
+    let a = 1
+    let b = 2
+
+    // Act: gọi hàm sum với tham số a và b
+    let result = sum(a, b)
+
+    // Assert: so sánh kết quả trả về từ hàm sum với kết quả mong đợi
+    let resultExpected = a + b
+    expect(result).toBe(resultExpected)
 })
